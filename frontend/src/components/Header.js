6f5f55c40dd6eef16444c9b6de295a72864cc0e3
VDrogobytskyy <volodymyrdrg@gmail.com>
2026-05-08 00:42:46 +0300

    LinkContainers implementing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
+
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Web Calorie Tracker</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Web Calorie Tracker</Navbar.Brand>
+                    </LinkContainer>
+                    
                     <div className="navbar-brand-centered" style={{
                         position: 'absolute',
                         left: '50%',
                         transform: 'translateX(-50%)',
                         zIndex: 10
                     }}>
-                        <a href="/">
+                        <Link to="/">
                             <img 
                                 src="/favicon.ico" 
                                 width="40" 
@@ -21,13 +27,16 @@ function Header() {
                                 className="d-inline-block align-top" 
                                 alt="Logo" 
                             />
-                        </a>
+                        </Link>
                     </div>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
                             {/* <Nav.Link href="/Profile"><i className="fas fa-user-plus"></i>Sing up</Nav.Link> */}
-                            <Nav.Link href="/login"><i className="fas fa-user"></i>Sing in</Nav.Link>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className="fas fa-user"></i>Sing in</Nav.Link>
+                            </LinkContainer>
+                            
                             {/* <Nav.Link href="/Home"><i className="fas fa-home"></i>Home page</Nav.Link> */}
                         </Nav>
                     </Navbar.Collapse>
