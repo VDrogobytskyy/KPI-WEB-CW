d43942ffb05c86d0129a69eab65baa0e16d39ae1
VDrogobytskyy <volodymyrdrg@gmail.com>
2026-05-11 00:38:02 +0300

    a bit work with JWT

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('foods/search/', views.foodSearch, name='food-search'),
+    path('users/profile/', views.getUserProfile, name='user-profile'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -9,6 +9,35 @@ from urllib.parse import urlencode
 from urllib.request import urlopen, Request
 import json
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+from .serializers import UserSerializer, UserSerializerWithToken
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
